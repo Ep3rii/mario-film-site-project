d906679b65acf5805de270b4580e7bdbfc0c648c
Euzébio Pereira da Silva Filho <euzzbs@gmail.com>
2023-01-26 04:32:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const video = document.getElementById('video');
+const linkDoVideo = video.src;
+const botaoTrailer = document.querySelector('.botao-trailer');
+const modal = document.querySelector('.modal');
+const botaoFecharModal = document.querySelector('.fechar-modal');
+
+botaoTrailer.addEventListener('click', () => {
+    modal.classList.add('aberto');
+    video.setAttribute('src', linkDoVideo);
+});
+
+botaoFecharModal.addEventListener('click' , () => {
+    modal.classList.remove('aberto');
+    video.setAttribute('src', '');
+});
+
+
+
+
+
